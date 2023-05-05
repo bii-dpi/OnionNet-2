f38d0c0517a76c9f2d6bd6197a4df0d3e5827f1d
bii-dpi <76100319+bii-dpi@users.noreply.github.com>
2023-05-06 05:34:58 +0800

    ..

--- 00_create_proc_proteins.py
+++ 00_create_proc_proteins.py
@@ -1,34 +1,34 @@
 import os
 
+from pickle import dump
 from progressbar import progressbar
 
 
-def save_proc_protein(fname, path):
+def get_kept_lines(pdb_id, path):
 
+    kept_lines = ""
     with open(path) as f:
-        with open(f"data/proc_protein/{fname}", "w") as g:
-            for each in f:
-                if "ATOM" in each:
-                    g.writelines(each)
-                    _ = '''
-                elif 'HETATM' in each:
-                    a.writelines(each)
-                        '''
-                else:
-                    continue
+        for line in f:
+            if "ATOM" in line:
+                kept_lines += line
+
+    return {pdb_id: kept_lines}
 
 
 def save_proc_proteins(mode):
 
-    pdb_pairs = [(fname, f"../docking/data/{mode}/pdb/{fname}")
+    pdb_pairs = [(fname[:-4], f"../docking/data/{mode}/pdb/{fname}")
                  for fname in os.listdir(f"../docking/data/{mode}/pdb/")]
 
     for pair in progressbar(pdb_pairs):
-        save_proc_protein(*pair)
+        kept_lines_dict.update(get_kept_lines(*pair))
 
 
 os.system("mkdir data/")
-os.system("mkdir data/proc_protein/")
+kept_lines_dict = {}
 for mode in ("training", "testing"):
     save_proc_proteins(mode)
+
+with open("data/protein_lines.pkl", "wb") as f:
+    dump(kept_lines_dict, f)
 
