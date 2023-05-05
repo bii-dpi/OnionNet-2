062706dfd4f4af88ca2f913b1097431140935d36
bii-dpi <76100319+bii-dpi@users.noreply.github.com>
2023-05-05 16:57:45 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+CONDA_BASE=$(conda info --base)
+conda create -n onion python=3.8 -y
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+conda activate onion
+
+pip install tensorflow-gpu==2.3
+
+conda install numpy pandas scikit-learn=0.22.1 -y
+conda install -c conda-forge mdtraj openbabel -y
+
